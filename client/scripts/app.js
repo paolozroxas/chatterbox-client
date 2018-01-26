084af67b455b757097dd80d1793d6877474d13b4
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-01-26 14:34:59 -0800

    got a GET request working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,15 @@
-// YOUR CODE HERE:
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages/',
+  type: 'GET',
+  contentType: 'application/json',
+
+  success: function (data) {
+    console.log('chatterbox: Request Sent');
+    console.log(data);
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send request', data);
+  }
+});
